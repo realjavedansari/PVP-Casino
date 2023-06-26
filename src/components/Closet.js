947ab69e8f74e27b4dce4d11570f661f99bbc13c
thanostez <thanostez@gmail.com>
2023-06-26 16:18:02 +0530

    1

--- src/components/Closet.js
+++ src/components/Closet.js
@@ -1,41 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../Game.css';
 
 const ClosetGame = () => {
-    const [characters, setCharacters] = useState([
-      { id: 'biden', name: 'Loser: Joe Biden', show: false },
-      { id: 'pelosi', name: 'Loser: Nancy Pelosi', show: false },
-      { id: 'clinton', name: 'Loser: Bill Clinton', show: false },
-      { id: 'trump', name: 'Winner: Donald Trump', show: false }
-    ]);
-  
-    const handleDoorClick = (closetId) => {
-      const updatedCharacters = characters.map((character) => {
-        if (character.id === closetId) {
-          return { ...character, show: true };
-        } else {
-          return { ...character, show: false };
-        }
-      });
-      setCharacters(updatedCharacters);
-    };
-  
-    return (
-      <div className="container">
-        {characters.map((character, index) => (
-          <div className="closet" key={`closet${index + 1}`}>
-            <div
-              className={`door ${character.show ? 'door-open' : ''}`}
-              onClick={() => handleDoorClick(character.id)}
-            ></div>
-            <div className={`character ${character.show ? 'slideIn' : ''}`}>
-              {character.show && character.name}
-            </div>
-          </div>
-        ))}
-      </div>
+  const [characters, setCharacters] = useState([
+    { id: 'biden', name: 'Loser: Joe Biden', show: false },
+    { id: 'pelosi', name: 'Loser: Nancy Pelosi', show: false },
+    { id: 'clinton', name: 'Loser: Bill Clinton', show: false },
+    { id: 'trump', name: 'Winner: Donald Trump', show: false }
+  ]);
+
+  const handleDoorClick = (closetId) => {
+    const updatedCharacters = characters.map((character) =>
+      character.id === closetId ? { ...character, show: true } : { ...character, show: false }
     );
+    setCharacters(updatedCharacters);
   };
-  
-  export default ClosetGame;
-  
+
+  return (
+    <div className="container">
+      {characters.map((character) => (
+        <div className="closet" key={character.id}>
+          <div
+            className={`door ${character.show ? 'door-open' : ''}`}
+            onClick={() => handleDoorClick(character.id)}
+          ></div>
+          <div className={`character ${character.show ? 'slideIn' : ''}`}>
+            {character.show && character.name}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ClosetGame;
